fad3387219f1ecc816d87770cd0021e5d0916df9
BraydenLinsangan <159149939+BraydenLinsangan@users.noreply.github.com>
2024-02-28 13:48:48 +0000

    added cool

---
+++ welcome.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+  <body>
+        Why hello there! <?php echo $_POST["name"]; ?><br>
+        Your cool snazzy looking email is: <?php echo $_POST["email"]; ?>
+
+  </body>
+</html>
